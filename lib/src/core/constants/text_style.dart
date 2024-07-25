328615f93e5c0e63cf6c81f219be3349553ddec6
Mohd Ryan Obillah <ryanobillah@gmail.com>
2024-07-25 08:50:06 +0700

    fix: add responsive

--- lib/src/core/constants/text_style.dart
+++ lib/src/core/constants/text_style.dart
@@ -1,6 +1,7 @@
 library text_style;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 //
@@ -16,42 +17,42 @@ class AppTextStyle extends TextStyle {
   // Header styles
   static TextStyle get headline => GoogleFonts.poppins(
         fontWeight: Font.b,
-        fontSize: 24.0,
+        fontSize: 24.0.sp,
       );
 
   static TextStyle get subtitle1 => GoogleFonts.poppins(
         fontWeight: Font.b,
-        fontSize: 14.0,
+        fontSize: 14.0.sp,
       );
 
   static TextStyle get subtitle2 => GoogleFonts.poppins(
         fontWeight: Font.b,
-        fontSize: 12.0,
+        fontSize: 12.0.sp,
       );
 
   static TextStyle get subtitle3 => GoogleFonts.poppins(
         fontWeight: Font.b,
-        fontSize: 10.0,
+        fontSize: 10.0.sp,
       );
 
   // Body styles
   static TextStyle get body1 => GoogleFonts.poppins(
         fontWeight: Font.n,
-        fontSize: 14.0,
+        fontSize: 14.0.sp,
       );
 
   static TextStyle get body2 => GoogleFonts.poppins(
         fontWeight: Font.n,
-        fontSize: 12.0,
+        fontSize: 12.0.sp,
       );
 
   static TextStyle get body3 => GoogleFonts.poppins(
         fontWeight: Font.n,
-        fontSize: 10.0,
+        fontSize: 10.0.sp,
       );
 
   static TextStyle get caption => GoogleFonts.poppins(
         fontWeight: Font.n,
-        fontSize: 8.0,
+        fontSize: 8.0.sp,
       );
 }
